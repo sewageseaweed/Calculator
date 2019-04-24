24281225138f38c80557af6ce8079596239fe6f5
Clyde Sumagang <reisumagang@gmail.com>
2019-04-24 13:34:52 -0700

    init commit

---
+++ testing.py
@@ -0,0 +1,65 @@
+from Tkinter import *
+
+root = Tk()
+root.title("Calculator")
+root.geometry("400x700")
+
+values = []
+
+
+def printName():
+    for i in range(len(values)):
+        print(values[i])
+
+
+total = Label(root, width=10, height=7)
+
+button1 = Button(text="1", width=5, height=8, command=lambda: total.configure(text=values))
+
+button2 = Button(text="2", width=5, height=8, command=lambda: total.configure(text="2"))
+
+button3 = Button(text="3", width=5, height=8, command=lambda: values.append(3))
+
+button4 = Button(text="4", width=5, height=8, command=lambda: values.append(4))
+
+button5 = Button(text="5", width=5, height=8, command=lambda: values.append(5))
+
+button6 = Button(text="6", width=5, height=8, command=lambda: values.append(6))
+
+button7 = Button(text="7", width=5, height=8, command=lambda: values.append(7))
+
+button8 = Button(text="8", width=5, height=8, command=lambda: values.append(8))
+
+button9 = Button(text="9", width=5, height=8, command=lambda: values.append(9))
+
+button0 = Button(text="0", width=5, height=8, command=lambda: values.append(0))
+
+button_plus = Button(text="+", width=5, height=8, command=lambda: printName())
+
+button_minus = Button(text="-", width=5, height=8)
+
+
+button1.grid(row=1, column=0, sticky="sew", pady=1)
+button2.grid(row=1, column=1, sticky="sew", pady=1)
+button3.grid(row=1, column=2, sticky="sew", pady=1)
+button4.grid(row=2, column=0, sticky="nsew", pady=1)
+button5.grid(row=2, column=1, sticky="nsew", pady=1)
+button6.grid(row=2, column=2, sticky="nsew", pady=1)
+button7.grid(row=3, column=0, sticky="nsew", pady=1)
+button8.grid(row=3, column=1, sticky="nsew", pady=1)
+button9.grid(row=3, column=2, sticky="nsew", pady=1)
+button0.grid(row=4, column=1, sticky="nsew", pady=1)
+button_plus.grid(row=4, column=0, sticky="nsew", pady=1)
+button_minus.grid(row=4, column=2, sticky="nsew", pady=1)
+
+total.grid(row=0, column=0, sticky="nsew")
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+root.rowconfigure(0, weight=2)
+root.rowconfigure(1, weight=1)
+root.rowconfigure(2, weight=1)
+root.rowconfigure(3, weight=1)
+
+root.mainloop()
